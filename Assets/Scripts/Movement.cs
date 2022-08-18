4b8b6061cef0906560f9ab2b34b8466d27cb9b02
FluffyMunda <kshitijyenurkar@gmail.com>
2022-08-18 14:38:00 +0530

    Uploading my project to Git

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotateSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+    public bool isOnGround = true;
+    private float forwardInput;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+
+}       
+
+
