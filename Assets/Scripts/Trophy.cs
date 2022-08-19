485d379adf83677e28bc6349c70bdecdc1b29d5d
FluffyMunda <kshitijyenurkar@gmail.com>
2022-08-19 23:52:47 +0530

    Uploading latest changes

--- Assets/Scripts/Trophy.cs
+++ Assets/Scripts/Trophy.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Trophy : MonoBehaviour
 {
     public GameObject winningScreenObject;
-   
+    public GameObject characterObject;
+    public GameObject secondcamObject;
     private void OnTriggerEnter(Collider other)
     {
         winningScreenObject.SetActive(true);
+        characterObject.SetActive(false);
+        secondcamObject.SetActive(true);
     }
 }
