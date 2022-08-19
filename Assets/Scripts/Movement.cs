4117a22b6cbd1940b155f67816859c72e689d190
FluffyMunda <kshitijyenurkar@gmail.com>
2022-08-19 14:29:10 +0530

    Uploading latest changes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -18,7 +18,7 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.W))
         {
